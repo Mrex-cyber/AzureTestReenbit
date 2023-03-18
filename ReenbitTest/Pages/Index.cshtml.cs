92e7b8c6bc8b539c18466b5464823cb5221d6182
Valentyn <rabincakv@gmail.com>
2023-03-18 20:54:41 +0200

    MainProject
    I made a main project.

---
+++ ReenbitTest/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ReenbitTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet(FileStream stream, string email)
+        {
+
+        }
+    }
+}
