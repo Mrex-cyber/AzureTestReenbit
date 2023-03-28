3902ff90991095d36ddd649d02c52c4928ce73a0
Valentyn <rabincakv@gmail.com>
2023-03-28 21:49:05 +0300

    Main tasks
    I added checking for a type of file.

--- ReenbitTest/Pages/Index.cshtml.cs
+++ ReenbitTest/Pages/Index.cshtml.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Azure.Storage.Blobs;
+using System.IO;
+using System.IO.Pipes;
 
 namespace ReenbitTest.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet(FileStream stream, string email)
+        public string blobStorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=emailnotificationr188986;AccountKey=3lAJgWjctRD8SIDUTIh9ew28jRaPZAX5UV0BEiFUik2uy7rcQQpWLWR83hCPkRSCvxybQ2JU7MVl+AStR+DHUQ==;EndpointSuffix=core.windows.net";
+        public string blobStorageContainerName = "blob-reenbit-storage";
+        
+        public async void OnPost(IFormFile uploadedFile, string email)
         {
+            BlobContainerClient container = new BlobContainerClient(blobStorageConnectionString, blobStorageContainerName);
+            string fileExt = System.IO.Path.GetExtension(uploadedFile.FileName);
 
-        }
+            if (uploadedFile != null && (fileExt == ".docx" || fileExt == ".doc"))
+            {
+                var blob = container.GetBlobClient(uploadedFile.FileName);
+                await blob.UploadAsync(uploadedFile.OpenReadStream());          
+            }
+        }        
     }
+
 }
